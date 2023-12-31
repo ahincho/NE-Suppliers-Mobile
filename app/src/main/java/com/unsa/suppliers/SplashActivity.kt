5b121c4ff7c5d2ec5fb2279a5e351409004355c0
ahincho <ahincho@unsa.edu.pe>
2023-12-30 21:26:04 -0500

    Adding Splash Screen

---
+++ app/src/main/java/com/unsa/suppliers/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.unsa.suppliers
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private val delay: Long = 5000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, AuthActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, this.delay)
+    }
+}
