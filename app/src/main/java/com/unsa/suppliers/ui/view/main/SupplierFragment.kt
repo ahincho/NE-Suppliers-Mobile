9a4175cd2ad711c86f270c4ae1209931667f7ce1
ahincho <ahincho@unsa.edu.pe>
2024-01-09 09:19:25 -0500

    Supplier Fragment

--- app/src/main/java/com/unsa/suppliers/ui/view/main/SupplierFragment.kt
+++ app/src/main/java/com/unsa/suppliers/ui/view/main/SupplierFragment.kt
@@ -12,6 +12,7 @@ import com.unsa.suppliers.data.adapters.SupplierAdapter
 import com.unsa.suppliers.data.dtos.suppliers.SupplierResponse
 import com.unsa.suppliers.databinding.FragmentSupplierBinding
 import com.unsa.suppliers.ui.viewmodel.main.MainViewModel
+import java.text.FieldPosition
 
 class SupplierFragment : Fragment() {
     private var _binding: FragmentSupplierBinding? = null
@@ -30,16 +31,32 @@ class SupplierFragment : Fragment() {
         mainViewModel.getAllSuppliers()
         mainViewModel.suppliers.observe(viewLifecycleOwner) {
             Log.d("SUPPLIER FRAGMENT", (mainViewModel.suppliers.value ?: "ERROR").toString())
-            updateRecyclerView(mainViewModel.suppliers.value ?: emptyList())
+            initRecyclerView(mainViewModel.suppliers.value ?: emptyList())
         }
-        initListeners()
     }
-    private fun updateRecyclerView(suppliers: List<SupplierResponse>) {
-        adapter = SupplierAdapter(suppliers)
+    private fun initRecyclerView(suppliers: List<SupplierResponse>) {
+        adapter = SupplierAdapter (
+            suppliers = suppliers,
+            deleteListener = { position -> onSupplierDelete(position) },
+            inactivateListener = { position -> onSupplierInactivate(position) },
+            reactivateListener = { position -> onSupplierReactivate(position) }
+        )
         binding.suppliersRv.layoutManager = manager
         binding.suppliersRv.adapter = adapter
     }
-    private fun initListeners() {
-
+    private fun onSupplierDelete(position: Int) {
+        mainViewModel.suppliers.value?.get(position)?.let { mainViewModel.deleteSupplier(it.id) }
+        mainViewModel.getSupplierById(position)
+        adapter.notifyItemChanged(position)
+    }
+    private fun onSupplierInactivate(position: Int) {
+        mainViewModel.inactivateSupplier(position)
+        mainViewModel.getSupplierById(position)
+        adapter.notifyItemChanged(position)
+    }
+    private fun onSupplierReactivate(position: Int) {
+        mainViewModel.reactivateSupplier(position)
+        mainViewModel.getSupplierById(position)
+        adapter.notifyItemChanged(position)
     }
 }
